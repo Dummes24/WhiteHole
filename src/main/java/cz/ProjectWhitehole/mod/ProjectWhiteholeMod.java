3d66e5744032ee313c040a8f18618f1e9df83554
Gekocius <vranadaniel@gmail.com>
2014-12-23 23:21:05 +0100

    Version change

--- src/main/java/cz/ProjectWhitehole/mod/ProjectWhiteholeMod.java
+++ src/main/java/cz/ProjectWhitehole/mod/ProjectWhiteholeMod.java
@@ -10,7 +10,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = ProjectWhiteholeMod.MODID, version = ProjectWhiteholeMod.VERSION)
 public class ProjectWhiteholeMod {
 	public static final String MODID = "projectwhitehole";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.0.1";
     
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
