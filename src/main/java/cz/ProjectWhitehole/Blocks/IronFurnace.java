f6f3392611d9763d2a4f76bbc61948e25838dc24
dummes24 <dummes24@gmail.com>
2015-01-05 22:16:53 +0100

    Added TileEntityMachineBase

---
+++ src/main/java/cz/ProjectWhitehole/Blocks/IronFurnace.java
@@ -0,0 +1,13 @@
+package cz.ProjectWhitehole.Blocks;
+
+import cofh.api.energy.EnergyStorage;
+
+public class IronFurnace extends EnergyStorage{
+
+	public IronFurnace(int capacity, int maxReceive, int maxExtract) {
+		super(capacity, maxReceive, maxExtract);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
